688a4b204008222d4aeac859450b438948c462fa
CoBunn <154394317+hakumaru12@users.noreply.github.com>
2026-01-31 07:01:41 +0000

    修正: wait_ssid.sh内のtmuxコマンドのタイプミスを修正し、TARGET_IP変数を使用するように変更

--- wait_ssid.sh
+++ wait_ssid.sh
@@ -2,7 +2,7 @@
 
 TARGET_SSID="BF-HSKK-2.4G"
 MOMO_DIR="/home/haku12/momo"
-
+TARGET_IP="192.168.11.2"
 echo "=============================="
 echo "Waiting for Wi-Fi - $TARGET_SSID"
 echo "=============================="
@@ -25,8 +25,8 @@ echo "=============================="
 
 echo "Starting streaming"
 cd "$MOMO_DIR"
-if ! tmuc has-session -t momo 2>/dev/null; then
-	tmux new-session -d -s momo "gst-launch-1.0 v4l2src device=/dev/video0 io-mode=2 ! image/jpeg,width=1280,height=720,framerate=30/1 ! jpegdec ! videoconvert ! openh264enc bitrate=6000000 complexity=0 ! rtph264pay pt=96 ! udpsink host=192.168.11.2 port=5000 sync=false
+if ! tmux has-session -t momo 2>/dev/null; then
+	tmux new-session -d -s momo "gst-launch-1.0 v4l2src device=/dev/video0 io-mode=2 ! image/jpeg,width=1280,height=720,framerate=30/1 ! jpegdec ! videoconvert ! openh264enc bitrate=6000000 complexity=0 ! rtph264pay pt=96 ! udpsink host=$TARGET_IP port=5000 sync=false
 "
 fi
 echo "Streaming started!"
