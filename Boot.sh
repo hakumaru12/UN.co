1916e9a310f60a586054b22c2d36ee71b4582056
CoBunn <154394317+hakumaru12@users.noreply.github.com>
2026-02-03 06:13:55 +0000

    不要なファイルを削除し、GStreamerのインストール手順をmemo.txtに追加

--- Boot.sh
+++ Boot.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 TARGET_SSID="BF-HSKK-2.4G"
-MOMO_DIR="/home/haku12/momo"
+USR_NAME="haku12"
+MOMO_DIR="/home/$USR_NAME/"
+TARGET_IP="192.168.11.2"
 
 echo "=============================="
 echo "Waiting for Wi-Fi - $TARGET_SSID"
@@ -23,11 +25,12 @@ echo "SSID: $SSID"
 echo "IP Address: $IP_ADDR"
 echo "=============================="
 
-echo "Starting streaming by momo"
+echo "Starting streaming"
 cd "$MOMO_DIR"
 if ! tmux has-session -t momo 2>/dev/null; then
-	tmux new-session -d -s momo "./momo --no-audio-device p2p"
+	tmux new-session -d -s momo "gst-launch-1.0 v4l2src device=/dev/video0 io-mode=2 ! image/jpeg,width=1280,height=720,framerate=30/1 ! jpegdec ! videoconvert ! openh264enc bitrate=6000000 complexity=0 ! rtph264pay pt=96 ! udpsink host=$TARGET_IP port=5000 sync=false
+"
 fi
 echo "Streaming started!"
-echo "Starting raspi.py (RC control)"
-python3 /home/haku12/raspi.py
+echo "Starting raspy.py (RC controll)"
+python3 /home/$USR_NAME/raspi.py
